3ae2c4e43c01c7bfb14c181f747889d6be83530c
bin <1061015041@qq.com>
2016-04-05 14:22:02 +0800

    退出帐号部分弹窗已做，方法:显示隐藏weui弹窗

--- www/js/account/account.list.js
+++ www/js/account/account.list.js
@@ -13,27 +13,24 @@
   /* @ngInject */
   function AccountListCtrl($scope) {
     $scope.init = init;
-
+    //默认弹窗状态不显示（状态为0时不显示，为1显示）
+    $scope.dialogStatus = 0;
+    $scope.showDialog = showDialog;
+    $scope.hideDialog = hideDialog;
 
     init();
 
-    // A confirm dialog
-    $scope.showConfirm = function() {
-      var confirmPopup = $ionicPopup.confirm({
-        title: '确认',
-        template: 'Are you sure you want to eat this ice cream?'
-      });
-      confirmPopup.then(function(res) {
-        if(res) {
-          console.log('You are sure');
-        } else {
-          console.log('You are not sure');
-        }
-      });
-    };
-
-
     function init() {
+    }
+
+    //显示弹窗
+    function showDialog() {
+      $scope.dialogStatus = 1;
+    }
+
+    //隐藏弹窗
+    function hideDialog() {
+      $scope.dialogStatus = 0;
     }
 
   }
